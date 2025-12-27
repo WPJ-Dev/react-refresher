0e8500ff5448d4ad828bce6b583d3c2d51b2b3d1
WPJ-Dev <wpruittDev@gmail.com>
2025-12-26 19:18:29 -0600

    working on bookShelf app, basic app and search functionality created

--- bookshelf/src/App.jsx
+++ bookshelf/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import BookList from "./components/BookList";
+import fetchBooks from "./services/api-client";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //url: https://www.googleapis.com/books/v1/volumes?q=javascript/
+  const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("CSS");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, [searchTerm]);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const items = await fetchBooks(searchTerm);
+      setBooks(items);
+      // console.log(booksJson.items);
+      setIsLoading(false);
+    } catch (error) {
+      if (error.name !== "AbortError") {
+        console.error("Failed to fetch books:", error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+    // const res = await fetch(
+    //   `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}/`
+    // );
+    // const booksJson = await res.json();
+  };
+
+  function handleSearch(searchTerm) {
+    setSearchTerm(searchTerm);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <NavBar onSearch={handleSearch} />
+      {isLoading ? (
+        <p
+          class="placeholder-glow display-3 text-body-secondary"
+          aria-hidden="true"
+        >
+          <span class="placeholder col-6"></span>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      ) : (
+        <BookList books={books} />
+      )}
+      <Footer />
     </>
   );
 }
