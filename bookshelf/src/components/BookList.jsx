a0825efa6dc7ea9afaac9a4a6a3c6a864cb3b743
WPJ-Dev <wpruittDev@gmail.com>
2025-12-27 13:31:48 -0600

    abort controller added to for useBook/fetchBooks api call, null handling for bookList mapping -- likely need more null check handling due to react component lifecycle and multi network calls, outside of being good practice

--- bookshelf/src/components/BookList.jsx
+++ bookshelf/src/components/BookList.jsx
@@ -3,9 +3,7 @@ import BookCard from "./BookCard";
 function BookList({ books }) {
   return (
     <div className="row">
-      {books.map((book) => (
-        <BookCard key={book.id} book={book} />
-      ))}
+      {books && books.map((book) => <BookCard key={book.id} book={book} />)}
     </div>
   );
 }
--- bookshelf/src/services/useBooks.js
+++ bookshelf/src/services/useBooks.js
@@ -7,10 +7,12 @@ function useBooks(initialQuery) {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     async function fetchBooksData(query) {
       setIsLoading(true);
       try {
-        const items = await fetchBooks(query);
+        const items = await fetchBooks(query, controller);
         setBooks(items);
       } catch (error) {
         if (error.name !== "AbortError") {
@@ -24,6 +26,8 @@ function useBooks(initialQuery) {
     if (searchTerm) {
       fetchBooksData(searchTerm);
     }
+
+    return () => controller.abort();
   }, [searchTerm]);
 
   return { books, isLoading, setSearchTerm };
